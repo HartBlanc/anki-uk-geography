7e974bddf7931110c429ce2cbae3dcc96de996df
Callum Hart <callumhartemail@gmail.com>
2020-08-25 11:32:00 +0100

    refactor: type hints and docstrings for wikipedia methods

--- wikipedia.py
+++ wikipedia.py
@@ -1,117 +1,101 @@
-#!/usr/bin/python3
-
-"""
-    parse_wikitable.py
-
-    MediaWiki Action API Code Samples
-    Demo of `Parse` module: Parse a section of a page, fetch its table data and save
-    it to a CSV file
-
-    MIT license
-"""
+""" Methods for parsing information from wikipedia articles. """
 
 import csv
-import requests
-
-S = requests.Session()
-
-URL = "https://en.wikipedia.org/w/api.php"
-
-PARAMS = {
-    'action': "parse",
-    'prop': 'wikitext',
-    'section': 4,
-    'format': "json"
-}
-
-def get_table(page, filename):
-    """ Parse a section of a page, fetch its table data and save it to a CSV file
-    """
-    params = PARAMS.copy()
-    params['page'] = page
-    res = S.get(url=URL, params=params)
-    data = res.json()
-    wikitext = data['parse']['wikitext']['*']
-    lines = wikitext.split('|-')[2:]
-    entries = []
-
-    # print("hello", wikitext)
-    # print("hello2", lines)
-
-
-    for line in lines:
-        line = line.strip()
-        if line.startswith("|"):
-            entry = line.split("]")[0].replace("|[[", "")
-            entries.append([entry])
-
-    with open(filename, "w") as file:
-        writer = csv.writer(file)
-        writer.writerows(entries)
+from typing import List, Dict, Tuple
 
 from lxml import html, etree
+import requests
 
-def extract_all_links(url, xpath):
 
-    r = requests.get(url)
+def extract_links(wiki_url: str, link_xpath: str) -> List[Tuple[str, str]]:
+    """
+    :param wiki_url: URL for a wikipedia article to extract links from
+    :param link_xpath: the XPath for identifying which elements to extract
+    :return: a list of pairs of link texts and link URLs
+             (e.g. "Durham", "https://en.wikipedia.org/wiki/Durham,_England")
+    """
+
+    r = requests.get(wiki_url)
     r.raise_for_status()
+
     tree = html.fromstring(r.text)
-    links = tree.xpath(xpath)
-    rows = [(link.text, "https://en.wikipedia.org" + link.get('href')) for link in links]
+
+    link_elems = tree.xpath(link_xpath)
+    rows = [(link_elem.text, f"https://en.wikipedia.org{link_elem.get('href')}")
+            for link_elem in link_elems]
+
     return rows
 
-def city_to_county(city, lookup):
 
-    if 'constituent country' in lookup:
+def city_to_county(city: str, city_infobox: Dict[str, str]) -> str:
+    """
+    Returns the corresponding county that the city is in,
+    based on the wikipedia infobox from the article for that city.
+    Naming differences between articles are resolved and edge cases are handled.
+
+    :param city: the city for which the county is needed.
+    :param city_infobox: the infobox for the wikipedia article for this city
+    :return: The county of the city.
+    """
+
+    if 'constituent country' in city_infobox:
         country_type = 'constituent country'
     else:
         country_type = 'country'
 
-    if lookup[country_type] == 'Scotland':
-        return lookup['council area']
-    
-    elif lookup[country_type] in ("England", "Wales", "Northern Ireland"):
-        if "status" in lookup:
-            if "county" in lookup['status']:
+    if city_infobox[country_type] == 'Scotland':
+        return city_infobox['council area']
+
+    elif city_infobox[country_type] in ("England", "Wales", "Northern Ireland"):
+        if "status" in city_infobox:
+            if "county" in city_infobox['status']:
                 return city
-        if "ceremonial county" in lookup:
+
+        if "ceremonial county" in city_infobox:
             key = "ceremonial county"
         else:
-            for k in lookup:
+            for k in city_infobox:
                 if 'county' in k:
                     key = k
                     break
 
-        return lookup[key]
-    
+        return city_infobox[key]
 
-    print("ERROR", lookup)
+    print("ERROR", city_infobox)
 
-def get_infobox_items(url):
-    r = requests.get(url)
+
+def get_infobox_items(article_url: str) -> Dict[str, str]:
+    """
+    :param article_url: A URL for a Wikipedia article with an infobox
+    :return: a label->value dictionary of the rows of the infobox
+    """
+    r = requests.get(article_url)
     r.raise_for_status()
     tree = html.fromstring(r.text)
     
-    infobox = tree.xpath("//table[contains(concat(' ',normalize-space(@class),' '),' infobox ')]")[0]
-    label_elems = infobox.xpath('.//th[@scope="row"]')
-    value_elems = infobox.xpath('.//th[@scope="row"]/following-sibling::td[1]')
+    infobox_elem = tree.xpath("//table[contains(concat(' ',normalize-space(@class),' '),' infobox ')]")[0]
+    label_elems = infobox_elem.xpath('.//th[@scope="row"]')
+    value_elems = infobox_elem.xpath('.//th[@scope="row"]/following-sibling::td[1]')
 
     labels = [' '.join(elem.xpath('.//text()')).lower().replace("\xa0", " ").strip() for elem in label_elems]
     values = [' '.join(elem.xpath('.//text()')).replace("\xa0", " ").strip() for elem in value_elems]
     assert (len(labels) == len(values))
-    lookup = {label: value for (label, value) in zip(labels, values)} 
-    return lookup
+
+    return {label: value for (label, value) in zip(labels, values)}
+
 
 if __name__ == '__main__':
-    # get_table("Ceremonial_counties_of_England", "counties.csv")
 
-    urls = extract_all_links("https://en.wikipedia.org/wiki/List_of_cities_in_the_United_Kingdom",'/html/body/div[3]/div[3]/div[4]/div/div[5]/div/div[1]/div//a')
+    city_urls = extract_links('https://en.wikipedia.org/wiki/List_of_cities_in_the_United_Kingdom',
+                              '/html/body/div[3]/div[3]/div[4]/div/div[5]/div/div[1]/div//a')
 
     city_county = []
-    for (city, url) in urls:
-        infobox = get_infobox_items(url)
-        print("City:", f"{city},", "County:", city_to_county(city, infobox))
-        city_county.append((city, city_to_county(city, infobox)))
+    for (city, url) in city_urls:
+
+        infobox_map = get_infobox_items(url)
+        county = city_to_county(city, infobox_map)
+        print("City:", f"{city},", "County:", county)
+        city_county.append((city, county))
 
     with open("cities.csv", "w") as file:
         writer = csv.writer(file)
